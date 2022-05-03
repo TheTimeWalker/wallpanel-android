9c9e6973b79104d828c085a3bb6171b62176a5e0
George Medve <george.medve@gmail.com>
2022-05-03 13:24:19 +0100

    MQTTHive & Sensors were causing a OOM, added Garbage Collection. Appears to help

--- WallPanelApp/src/main/java/xyz/wallpanel/app/WallPanel.kt
+++ WallPanelApp/src/main/java/xyz/wallpanel/app/WallPanel.kt
@@ -16,7 +16,13 @@
 
 package xyz.wallpanel.app
 
+import android.R.attr
+import android.content.ComponentCallbacks2
 import android.content.Context
+import android.os.Process.myPid
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
 import androidx.multidex.MultiDex
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
@@ -25,6 +31,7 @@ import xyz.wallpanel.app.di.DaggerApplicationComponent
 import xyz.wallpanel.app.utils.CrashlyticsDebugTree
 import xyz.wallpanel.app.utils.LauncherShortcuts
 import xyz.wallpanel.app.utils.WallpanelDebugTree
+
 
 class WallPanel : DaggerApplication() {
 
@@ -42,11 +49,68 @@ class WallPanel : DaggerApplication() {
             Timber.plant(CrashlyticsDebugTree())
 //            Timber.plant(CrashlyticsTree())
         }
+        strictMode()
         LauncherShortcuts.createShortcuts(this)
+    }
+
+    private fun strictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork() // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build()
+            )
+        }
     }
 
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
         MultiDex.install(this)
     }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        System.gc()
+    }
+
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+        when (level) {
+            ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN -> {
+                Timber.v("The user interface has moved to the background.")
+                Runtime.getRuntime().gc()
+            }
+            ComponentCallbacks2.TRIM_MEMORY_RUNNING_MODERATE,
+            ComponentCallbacks2.TRIM_MEMORY_RUNNING_LOW,
+            ComponentCallbacks2.TRIM_MEMORY_RUNNING_CRITICAL -> {
+                Timber.v("If the event is TRIM_MEMORY_RUNNING_CRITICAL, then the system will begin killing background processes.")
+                Runtime.getRuntime().gc()
+            }
+            ComponentCallbacks2.TRIM_MEMORY_BACKGROUND,
+            ComponentCallbacks2.TRIM_MEMORY_MODERATE,
+            ComponentCallbacks2.TRIM_MEMORY_COMPLETE -> {
+                Timber.v("If the event is TRIM_MEMORY_COMPLETE, the process will be one of the first to be terminated.")
+                //val processId: Int = Process.myPid()
+                //Process.killProcess(processId)
+                Runtime.getRuntime().gc()
+            }
+            else -> {
+                Timber.v("The app received an unrecognized memory level value from the system. Treat this as a generic low-memory message.")
+                throw IllegalStateException("Unexpected value: $level")
+            }
+        }
+    }
+
+
 }
